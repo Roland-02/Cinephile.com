a9a48a9fe3bb744b96a43535118ef665122d764a
O Olajide <oo169@student.le.ac.uk>
2023-12-27 20:12:56 +0000

    fiexed html/ejs routing

--- webpage/routes/index.js
+++ webpage/routes/index.js
@@ -2,11 +2,12 @@
 
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 
 //get request - open index.ejs page
 router.get(['/', '/home', '/index', '/homepage'], function (req, res) {
     res.render('index', { title: 'Express', session: {email : req.cookies.sessionEmail }} );
-
+    
 });
 
 router.post('/signout', function (req, res) {
