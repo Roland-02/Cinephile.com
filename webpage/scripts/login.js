502fe6cfa9374098432a762c26e83f04c12b86af
O Olajide <oo169@student.le.ac.uk>
2023-12-27 20:22:48 +0000

    simple login/create account

---
+++ webpage/scripts/login.js
@@ -0,0 +1,40 @@
+//js scripting for createAccount page
+window.onload = function () {
+    const form = document.getElementById('login-form');
+
+    //event listener for form submission
+    form.addEventListener('submit', function (event) {
+        event.preventDefault(); // Prevent form submission
+
+        //frontend validation
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
+
+        // Perform your validation logic here
+        if (!validateEmail(email) || !validatePassword(password)) {
+            window.alert('Credentials invalid');
+            return;
+        }
+
+        //allow form submission
+        form.submit();
+        
+    });
+};
+
+
+function validateEmail(email) {
+    let valid = false;
+
+    //matches email format
+    const regex = /\S+@\S+\.\S+/;
+    if (regex.test(email)){
+        valid = true;
+    }
+
+    return valid;
+}
+
+function validatePassword(password) {
+    return (password && password.trim().length !== 0);
+}
