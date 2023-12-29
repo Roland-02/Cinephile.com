6662ff414cc98c0e5711702d41e7e84f64470f09
O Olajide <oo169@student.le.ac.uk>
2023-12-29 21:36:28 +0000

    started putting film data on frontend

--- webpage/routes/index.js
+++ webpage/routes/index.js
@@ -2,12 +2,23 @@
 
 var express = require('express');
 var router = express.Router();
-const path = require('path');
+const fs = require('fs');
+const csvParser = require('csv-parser');
 
 //get request - open index.ejs page
 router.get(['/', '/index', '/discover', '/home'], function (req, res) {
-    res.render('index', { title: 'Express', session: {email : req.cookies.sessionEmail }} );
+
+    const films = [];
+
+    fs.createReadStream('film_data.csv').pipe(csvParser()).on('data', (row) => {
+        films.push(row);
+    })
+    .on('end', () => {
+        res.render('index', { title: 'Express', session: { email: req.cookies.sessionEmail }, films: films });
+    })
+
 });
+
 
 router.post('/signout', function (req, res) {
     res.clearCookie('sessionEmail')
