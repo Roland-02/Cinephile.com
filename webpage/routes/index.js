8426984a09d55179a79089d5e3c2e9f681ff71b5
O Olajide <oo169@student.le.ac.uk>
2024-01-01 15:26:38 +0000

    progress with clicking through films

--- webpage/routes/index.js
+++ webpage/routes/index.js
@@ -5,32 +5,29 @@ var router = express.Router();
 const fs = require('fs');
 const csv = require('csvtojson');
 
-const csvParser = require('csv-parser');
-const { endianness } = require('os');
 
-
-
-
-
-
-
-//get request - open index.ejs page
-router.get(['/', '/index', '/discover', '/home'], function (req, res) {
-
-    /*
+function csvTojson() {
     csv()
         .fromFile('film_data.csv')
         .then((jsonArrayObj) => {
             fs.writeFileSync('film_data.json', JSON.stringify(jsonArrayObj, null, 2));
         })
         .catch((err) => console.error(err));
-        */
+}
+
+
+//get request - open index.ejs page
+router.get(['/', '/index', '/discover', '/home'], function (req, res) {
+
+    // Read the file synchronously (you can also use asynchronous methods if needed)
+    const file = fs.readFileSync('film_data.json', 'utf-8');
+
+    // Parse the JSON content into a JavaScript object
+    const films = JSON.parse(file);
+    console.log(films);
 
     //read film data into array for frontend
-    const films = require('film_data.json');
-
     res.render('index', { title: 'Express', session: { email: req.cookies.sessionEmail }, films: films });
-        
 
 });
 
