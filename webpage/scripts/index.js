28cf4525f7f6e7ce37f8fa29075af688f9271008
O Olajide <oo169@student.le.ac.uk>
2024-01-01 15:26:38 +0000

    progress with clicking through films

--- webpage/scripts/index.js
+++ webpage/scripts/index.js
@@ -3,32 +3,33 @@ window.onload = function () {
     const prevButton = document.getElementById('prev-btn');
     const nextButton = document.getElementById('next-btn');
 
-     
-       var films = JSON.parse(document.getElementById('film-carousel').getAttribute('data'));
-       console.log(films)
-       var currentIndex = 0;
-   
-       // Initial update
-       updateFilm();
-   
-       // Function to update the displayed film
-       function updateFilm() {
-           currentFilmElement.innerHTML = `<strong>Title:</strong> ${films[currentIndex].primaryTitle} <br><p></p>`;
-       }
-   
-       // Event listener for the previous button
-       prevButton.addEventListener('click', function () {
-           currentIndex = (currentIndex - 1);
-           if(currentIndex < 0){
-               currentIndex = 0;
-           }
-           updateFilm();
-       });
-   
-       // Event listener for the next button
-       nextButton.addEventListener('click', function () {
-           currentIndex = (currentIndex + 1);
-           updateFilm();
-       });
-   
+
+    var films = JSON.parse(document.getElementById('film-carousel').getAttribute('data'));
+    console.log(films);
+    //var films = document.getElementById('film-carousel').getAttribute('data');
+    var currentIndex = 0;
+
+    // Initial update
+    updateFilm();
+
+    // Function to update the displayed film
+    function updateFilm() {
+        currentFilmElement.innerHTML = `<strong>Title:</strong> ${films[currentIndex].primaryTitle} <br><p></p>`;
+    }
+
+    // Event listener for the previous button
+    prevButton.addEventListener('click', function () {
+        currentIndex = (currentIndex - 1);
+        if (currentIndex < 0) {
+            currentIndex = 0;
+        }
+        updateFilm();
+    });
+
+    // Event listener for the next button
+    nextButton.addEventListener('click', function () {
+        currentIndex = (currentIndex + 1);
+        updateFilm();
+    });
+
 };
