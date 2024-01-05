18368c7928d4239d166d76cfd29372a7ba016fc8
O Olajide <oo169@student.le.ac.uk>
2024-01-05 23:50:23 +0000

    adjusted poster styling, added film plots

--- webpage/scripts/index.js
+++ webpage/scripts/index.js
@@ -1,6 +1,5 @@
-//theMovieDB stuff
-
- function Fetch(film_id) {
+//theMovieDB api call for film poster
+function FetchImage(film_id) {
     const url = `https://api.themoviedb.org/3/movie/${film_id}/images`;
     const options = {
         method: 'GET',
@@ -19,6 +18,26 @@
         });
 };
 
+//theMovieDB api call for film data
+function FetchData(film_id) {
+    const url = `https://api.themoviedb.org/3/movie/${film_id}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YmYxZTkxOWFjMDBkYmI2NjhjODVlODg5ZWJjZTg1ZCIsInN1YiI6IjY1OGIwNzEyMzI1YTUxNTkyNzAxNWU4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yKZIOsVYvJxzRO3GJ1yayqvSCZg3l-ryO9FjBkfHIZc'
+        }
+    };
+    return fetch(url, options)
+        .then(res => {
+            return res.json();
+        })
+        .catch(err => {
+            console.error('Error fetching data:', err);
+            return null;
+        });
+
+}
 
 window.onload = function () {
     const filmTitle = document.getElementById('film-title');
@@ -26,27 +45,42 @@ window.onload = function () {
     const prevButton = document.getElementById('prev-btn');
     const nextButton = document.getElementById('next-btn');
 
+    //read in film data file
     var films = JSON.parse(document.getElementById('film-carousel').getAttribute('data'));
     var currentIndex = 0;
 
+    //for getting film poster jpegs
     const baseImagePath = 'https://image.tmdb.org/t/p/w500';
 
-    // Initial update
+    //Initial update
     updateFilm();
 
-    // Function to update the displayed film
+    //Function to update the displayed film
     async function updateFilm() {
-        filmTitle.innerHTML = `<strong>${films[currentIndex].primaryTitle}</strong> <br><p></p>`;
+        var filmData = await FetchData(films[currentIndex].tconst);
+        var plot = filmData.overview;
+        var title = filmData.title;
 
-        var filmImage = await Fetch(films[currentIndex].tconst);
+        //display film title
+        filmTitle.innerHTML = `<strong>${title}</strong> <br>`;
 
+        //display film plot summary
+        if (plot) {
+            filmTitle.innerHTML += `<div class="text-sm-center"> <p> ${plot} </p> </div>`;
+        } else {
+            filmTitle.innerHTML += `<div> <p> - </p> </div>`;
+        }
+
+
+        //display film poster
+        var filmImage = await FetchImage(films[currentIndex].tconst);
         if (filmImage && filmImage.posters && filmImage.posters.length > 0) {
             var imagePath = baseImagePath + filmImage.posters[0].file_path;
             filmPoster.innerHTML = `<img src="${imagePath}" alt="${films[currentIndex].primaryTitle}">`;
         } else {
-            filmPoster.innerHTML = '<p>Poster not available</p>';
+            filmPoster.innerHTML = `<img src="/images/MissingPoster.jpeg" alt="Poster Not Available">`;
         }
-        
+
     }
 
 
