2e0692433ac1ffe4df711fb555f1b026295c4e37
O Olajide <oo169@student.le.ac.uk>
2024-05-02 04:34:25 +0100

    added sql migration files

---
+++ mysql_schema_tables/users_user_liked_cast.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for macos12 (x86_64)
+--
+-- Host: 127.0.0.1    Database: users
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `user_liked_cast`
+--
+
+LOCK TABLES `user_liked_cast` WRITE;
+/*!40000 ALTER TABLE `user_liked_cast` DISABLE KEYS */;
+INSERT INTO `user_liked_cast` VALUES (56,'tt0317219',' Paul Newman'),(56,'tt0975676',' Max Herbrechter'),(56,'tt1204979',' Gwendoline Yeo'),(57,'tt0058262',' Angie Dickinson'),(57,'tt0076090',' Hideji Ôtaki'),(57,'tt0076090',' Kiwako Taichi'),(57,'tt0076090',' Kôji Ishizaka'),(57,'tt0076090',' Mitsuko Kusabue'),(57,'tt0076090',' Reiko Ôhara'),(57,'tt0076090',' Ryôko Sakaguchi'),(57,'tt0076090',' Takeshi Katô'),(57,'tt0076090',' Tatsuo Matsumura'),(57,'tt0076090',' Yûko Asano'),(57,'tt0076090','Yôko Tsukasa'),(57,'tt0109028',' Fisher Stevens'),(57,'tt0109028',' Gísli Halldórsson'),(57,'tt0109028',' Hiromasa Shimada'),(57,'tt0109028',' Laura Hughes'),(57,'tt0109028',' Lili Taylor'),(57,'tt0109028',' Masatoshi Nagase'),(57,'tt0109028',' Masayuki Sasaki'),(57,'tt0109028',' Seijun Suzuki'),(57,'tt0109028',' Taizô Mizumura'),(57,'tt0109028','Ichiko Takashi'),(57,'tt0114924',' Ally Walker'),(57,'tt0114924',' Glynis Johns'),(57,'tt0114924',' Jack Warden'),(57,'tt0114924',' Jason Bernard'),(57,'tt0114924',' Michael Rispoli'),(57,'tt0114924',' Micole Mercurio'),(57,'tt0114924',' Peter Boyle'),(57,'tt0114924',' Peter Gallagher'),(57,'tt0114924',' Sandra Bullock'),(57,'tt0137452',' Christina Lucia Peralta-Ramos'),(57,'tt0137452',' Edward Sanchez'),(57,'tt0137452',' Jack Senior'),(57,'tt0137452',' Jeff Jenkins'),(57,'tt0137452',' Jimmy Williams'),(57,'tt0137452',' Lonnie Schuyler'),(57,'tt0137452',' Michael M. Foley'),(57,'tt0137452',' Sandy Palm'),(57,'tt0137452',' Stephanie Warner'),(57,'tt0137452','William Smith'),(57,'tt0258967',' Brendan Fletcher'),(57,'tt0258967',' Chelsea Field'),(57,'tt0258967',' Dena Lang'),(57,'tt0258967',' Linda Cardellini'),(57,'tt0258967',' Peter Hanlon'),(57,'tt0258967',' Teri Polo'),(57,'tt0258967',' Trevor Aikman'),(57,'tt0258967',' Trevor Blumas'),(57,'tt0258967',' Vincent Kartheiser'),(57,'tt0258967','Andy Garcia'),(57,'tt0489237',' Scarlett Johansson'),(57,'tt1790864',' Aml Ameen'),(57,'tt1790864',' Chris Sheffield'),(57,'tt1790864',' Kaya Scodelario'),(57,'tt1790864',' Thomas Brodie-Sangster'),(57,'tt1790864',' Will Poulter'),(57,'tt1790864','Dexter Darden'),(57,'tt2511428',' Bryan Cranston'),(57,'tt2511428',' Robin Lord Taylor'),(57,'tt2511428',' Sarah Sokolovic'),(57,'tt2511428',' Ursula Parker'),(57,'tt6958520',' Alaina Garland');
+/*!40000 ALTER TABLE `user_liked_cast` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-02  4:25:04
