d2726943b0ce114bd8953bc460ccca112c68e28b
O Olajide <oo169@student.le.ac.uk>
2024-10-06 02:01:08 +0100

    file rearrange

---
+++ routes/search.js
@@ -0,0 +1,26 @@
+//handle GET request for /home, load film data
+var express = require('express');
+const router = express.Router();
+const axios = require("axios");
+const filmsRouter = require('./films');
+router.use('/routes', filmsRouter);
+
+
+router.get(['/', '/search'], async function (req, res) {
+    try {
+        const queryName = req.query.query;
+        res.render('search', {
+            title: 'Express',
+            session: { email: req.cookies.sessionEmail, id: req.cookies.sessionID },
+            query: queryName
+        });
+
+    } catch (error) {
+        console.error("Error: ", error);
+    }
+
+});
+
+
+
+module.exports = router;
