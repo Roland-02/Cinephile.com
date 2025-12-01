77de23a5a884d585d4d760a14b0ef7306b5f9ff8
O Olajide <oo169@student.le.ac.uk>
2025-12-01 22:01:44 +0000

    navbar buttons design

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -28,9 +28,12 @@ const Navbar = () => {
         {/* Left elements */}
         <div className="nav-left">
           {isAuthenticated ? (
-            <form onSubmit={handleSignOut}>
-              <button type="submit" id="signoutBtn" className="nav-link d-inline-block">
-                <p className="mb-0">Sign out</p>
+            <form onSubmit={handleSignOut} className="d-inline-block">
+              <button type="submit" className="nav-link d-inline-block" style={{ border: 'none', padding: '5px', background: 'none' }}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="black" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z" />
+                  <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z" />
+                </svg>
               </button>
             </form>
           ) : (
@@ -44,8 +47,8 @@ const Navbar = () => {
                 viewBox="0 0 16 16"
                 style={{ cursor: 'pointer' }}
               >
-                <path fillRule="evenodd" strokeWidth="1.5" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"/>
-                <path fillRule="evenodd" strokeWidth="1.5" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+                <path fillRule="evenodd" strokeWidth="1.5" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z" />
+                <path fillRule="evenodd" strokeWidth="1.5" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
               </svg>
             </Link>
           )}
